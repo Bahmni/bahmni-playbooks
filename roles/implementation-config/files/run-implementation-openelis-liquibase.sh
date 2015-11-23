889735360ad795c877065145cef457cb2e031135
Preethi <preethi.ms34@gmail.com>
2015-11-23 16:39:16 +0530

    Preethi , Shruthi | Added seperate role for bahmni common conf and implementation config

---
+++ roles/implementation-config/files/run-implementation-openelis-liquibase.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e -x
+. /etc/bahmni-installer/bahmni.conf
+
+CHANGE_LOG_TABLE="-Dliquibase.databaseChangeLogTableName=databasechangelog -Dliquibase.databaseChangeLogLockTableName=databasechangeloglock"
+LIQUIBASE_JAR="/opt/bahmni-lab/migrations/liquibase/lib/liquibase-1.9.5.jar"
+DRIVER="org.postgresql.Driver"
+CHANGE_LOG_FILE="/var/www/bahmni_config/openelis/migrations/liquibase.xml"
+
+java $CHANGE_LOG_TABLE -jar $LIQUIBASE_JAR --driver=$DRIVER --changeLogFile=$CHANGE_LOG_FILE --defaultSchemaName=clinlims --url=jdbc:postgresql://$OPENELIS_DB_SERVER:5432/clinlims --username=$OPENELIS_DB_USERNAME --password=$OPENELIS_DB_PASSWORD update
