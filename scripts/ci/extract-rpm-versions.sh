36a0e2654398963f1df7c0c068a477e30e786414
Preethi <preethi.ms34@gmail.com>
2015-12-04 15:43:21 +0530

    Removing ci-scripts. Doing it directly in GO instead

--- scripts/ci/extract-rpm-versions.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-for file in rpms/*; do
-  name=${file##*/}
-  echo ${name%.noarch.rpm} | sed 's/-/_/g'| rev | sed 's/_/ :noisrev_/2' |  sed 's/_/-/1' | rev >> rpm_versions.yml
-done
--- scripts/ci/validate-playbooks.sh
+++
@@ -1,2 +0,0 @@
-#!bin/bash
-ansible-playbook --syntax-check *.yml -i local
