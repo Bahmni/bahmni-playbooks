e0f393e65824ba02eebbdcba574153e498842dc7
Preethi <preethi.ms34@gmail.com>
2015-12-07 17:24:09 +0530

    Preethi, Shruthi |  Restoring dump and implementation config if provided

---
+++ roles/bahmni-emr/files/restore_mysql_dump.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e -x
+. /etc/bahmni-installer/bahmni.conf
+
+RESULT=`mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD --skip-column-names -e "SHOW DATABASES LIKE 'openmrs'"`
+if [ "$RESULT" != "openmrs" ] ; then
+    echo "openmrs database not found... Restoring a base dump"
+    mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD < '/opt/bahmni-installer/bahmni-playbooks/deployment-artifacts/mysql_dump.sql'
+fi
+
