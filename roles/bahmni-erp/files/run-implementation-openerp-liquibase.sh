5095006f5037c16ac1e283f335a64f35323aaecc
Preethi <preethi.ms34@gmail.com>
2015-12-12 11:56:18 +0530

    Preethi | Again fixing a bug in config migrations :(

--- roles/bahmni-erp/files/run-implementation-openerp-liquibase.sh
+++ roles/bahmni-erp/files/run-implementation-openerp-liquibase.sh
@@ -8,7 +8,7 @@ DRIVER="org.postgresql.Driver"
 CHANGE_LOG_FILE="/var/www/bahmni_config/openerp/migrations/liquibase.xml"
 CLASSPATH="/etc/bahmni-erp-connect/openerp-atomfeed-service.war"
 
-if [ ! -f "$CHANGE_LOG_FILE" ]
+if [  -f "$CHANGE_LOG_FILE" ]
 then
 java $CHANGE_LOG_TABLE -jar $LIQUIBASE_JAR --classpath=$CLASSPATH --driver=$DRIVER --changeLogFile=$CHANGE_LOG_FILE --defaultSchemaName=openerp --url=jdbc:postgresql://$OPENERP_DB_SERVER:5432/openerp --username=$OPENERP_DB_USERNAME --password=$OPENERP_DB_PASSWORD update
 fi
