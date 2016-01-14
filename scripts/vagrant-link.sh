712d2f1d46c97edc25670065bdefa81b44917082
chethana <chethana@INchethana-2.local>
2016-01-14 12:55:08 +0530

    Chethan, Shruthi | Added vagrant link script.

---
+++ scripts/vagrant-link.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -x -e
+
+PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $PATH_OF_CURRENT_SCRIPT/vagrant_functions.sh
+#USER=jss
+USER=bahmni
+
+if [ "$#" == "0" ]; then
+	FOLDER="app"
+else
+	FOLDER="$1"
+fi
+
+run_in_vagrant -c "sudo rm -rf /opt/bahmni-installer/bahmni-playbooks"
+
+run_in_vagrant -c "sudo mkdir -p /opt/bahmni-installer/"
+run_in_vagrant -c "sudo ln -s /bahmni/bahmni-playbooks/ /opt/bahmni-installer/bahmni-playbooks"
+run_in_vagrant -c "sudo chown -h ${USER}:${USER} /opt/bahmni-installer/bahmni-playbooks"
+run_in_vagrant -c "sudo mkdir -p /opt/bahmni-installer/bin/"
+
+run_in_vagrant -c "sudo rm -rf /opt/bahmni-installer/bin/bahmni"
+run_in_vagrant -c "sudo ln -s /bahmni/bahmni-package/bahmni-installer/scripts/rpm/bahmni /usr/bin/bahmni"
+
+run_in_vagrant -c "sudo mkdir -p /etc/bahmni-installer/"
+run_in_vagrant -c "sudo rm -rf /etc/bahmni-installer/setup.yml"
+run_in_vagrant -c "sudo ln -s /bahmni/bahmni-playbooks/dev/setup.yml /etc/bahmni-installer/setup.yml"
+
+run_in_vagrant -c "sudo rm -rf /etc/bahmni-installer/dev"
+run_in_vagrant -c "sudo ln -s /bahmni/bahmni-playbooks/dev/dev /etc/bahmni-installer/dev"
+run_in_vagrant -c "sudo rm -rf /etc/bahmni-installer/rpm_revisions.yml"
+run_in_vagrant -c "sudo ln -s /bahmni/bahmni-playbooks/dev/rpm_revisions.yml /etc/bahmni-installer/rpm_revisions.yml"
