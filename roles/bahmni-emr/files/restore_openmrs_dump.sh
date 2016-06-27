df789b62128c4ed33e455dd307a794ad3217c356
hemanths <hemanths@hemanths.local>
2016-06-27 12:54:31 +0530

    Alagesan, Hemanth | renamed openmrs dump name. reordered the tasks. ability to provide sql while installing for erp and elis.

--- roles/bahmni-emr/files/restore_openmrs_dump.sh
+++ roles/bahmni-emr/files/restore_openmrs_dump.sh
@@ -5,6 +5,7 @@ set -e -x
 RESULT=`mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD --skip-column-names -e "SHOW DATABASES LIKE 'openmrs'"`
 if [ "$RESULT" != "openmrs" ] ; then
     echo "openmrs database not found... Restoring a base dump"
-    mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD < '/etc/bahmni-installer/deployment-artifacts/mysql_dump.sql'
+    mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE openmrs;"
+    mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD openmrs < '/etc/bahmni-installer/deployment-artifacts/openmrs_backup.sql'
 fi
 
---
+++ roles/bahmni-lab/files/restore_openelis_dump.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+. /etc/bahmni-installer/bahmni.conf
+
+RESULT_USER=`psql -U postgres -h$OPENELIS_DB_SERVER -tAc "SELECT count(*) FROM pg_roles WHERE rolname='clinlims';"`
+RESULT_DB=`psql -U postgres -h$OPENELIS_DB_SERVER -tAc "SELECT count(*) from pg_catalog.pg_database where datname='clinlims';"`
+
+if [ "$RESULT_USER" == "0" ]; then
+    echo "creating postgres user - clinlims with roles CREATEDB,NOCREATEROLE,SUPERUSER,REPLICATION"
+    createuser -Upostgres  -h$OPENELIS_DB_SERVER -d -R -s --replication clinlims;
+fi
+
+if [ "$RESULT_DB" == "0" ]; then
+    createdb -Uclinlims -h$OPENELIS_DB_SERVER clinlims;
+    psql -Uclinlims -h$OPENELIS_DB_SERVER clinlims < '/etc/bahmni-installer/deployment-artifacts/openelis_backup.sql'
+fi
