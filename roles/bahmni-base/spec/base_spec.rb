e34e11deb80ee2b1c8aa56df605260af444c45f5
sravanthi17 <sravanns@thoughtworks.com>
2016-07-27 18:37:20 +0530

    Sravanthi | Adding test cases for Bahmni-base role

---
+++ roles/bahmni-base/spec/base_spec.rb
@@ -0,0 +1,63 @@
+require 'spec_helper'
+require 'ansible_spec'
+require 'specinfra'
+
+describe package('libselinux-python'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
+
+describe file('/etc/yum.repos.d/bahmni.repo'), :if => os[:family] == 'redhat' do
+  it { should exist }
+end
+
+describe package('epel-release'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
+
+describe package('ntp'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
+
+describe file('/etc/bahmni-installer/setup.yml') do
+  its(:content_as_yaml) { should include('timezone') }
+end
+
+describe service('ntpd') do
+  it { should be_enabled }
+end
+
+describe service('ntpd') do
+  it { should be_running }
+end
+
+describe package('tree'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
+
+describe package('htop'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
+
+describe package('lsof'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
+
+describe package('wget'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
+
+describe package('unzip'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
+
+describe package('python-pip'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
+
+describe package('rsync'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
+
+describe package('policycoreutils'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
