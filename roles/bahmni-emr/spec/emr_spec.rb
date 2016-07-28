0d6f06dcc28aa6329cfcdea936f787b6f0e9ab3f
senthilr <senthilr@thoughtworks.com>
2016-07-28 12:53:52 +0530

    added test for ERP,ERP connect and emr

--- roles/bahmni-emr/spec/emr_spec.rb
+++ roles/bahmni-emr/spec/emr_spec.rb
@@ -1,12 +1,40 @@
 require 'spec_helper'
 require 'ansible_spec'
 require 'specinfra'
+require 'yaml'
 
-describe service('httpd'), :if => os[:family] == 'redhat' do
-  it { should be_enabled }
+describe file('/etc/yum.repos.d/mysql.repo'), :if => os[:family] == 'redhat' do
+  it { should exist }
+end
+
+describe package('bahmni-openmrs'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
+
+describe package('bahmni-emr'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
+
+describe file('/opt/openmrs/etc/openmrs.conf'), :if => os[:family] == 'redhat' do
+  it { should exist }
+end
+
+describe iptables, :if => os[:family] == 'redhat' do
+  it { should have_rule('-P INPUT ACCEPT').with_table('mangle').with_chain('INPUT') }
+end
+
+describe file('/etc/bahmni-installer/deployment-artifacts/modules/'), :if => os[:family] == 'redhat' do
+  it { should be_directory }
+end
+
+describe file('/var/www/cgi-bin/getversion.py'), :if => os[:family] == 'redhat' do
+  it { should exist }
+end
+
+describe service('openmrs'), :if => os[:family] == 'redhat' do
   it { should be_running }
 end
 
-describe port(80) do
+describe port (8050), :if => os[:family] == 'redhat' do
   it { should be_listening }
 end
---
+++ roles/bahmni-erp-connect/spec/erpconnect_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+require 'ansible_spec'
+require 'specinfra'
+require 'yaml'
+
+
+describe package('bahmni-erp-connect'), :if => os[:family] == 'redhat' do
+  it { should be_installed }
+end
+
+describe file('/opt/bahmni-erp-connect/etc/bahmni-erp-connect.conf'), :if => os[:family] == 'redhat' do
+  it { should exist }
+end
+
+describe file('/opt/bahmni-erp-connect/bahmni-erp-connect/WEB-INF/classes/atomfeed.properties'), :if => os[:family] == 'redhat' do
+  it { should exist }
+end
+
+describe service('bahmni-erp-connect'), :if => os[:family] == 'redhat' do
+  it { should be_running }
+end
+
+
+
+
+
+
+
