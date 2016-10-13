7ef8958976b359265b2a49a8c8eff053ebbb3c14
Shruthip <shruthi.pitta@gmail.com>
2016-10-13 13:00:10 +0530

    Shruthi & Banka| Allowing smaller subset of config for offline setup
    Offline implementors can override their config for offline use by
    placing the files in offline folder in corresponding path inside Bahmni config

--- roles/bahmni-concat-config/files/bahmni_concat_config.py
+++ roles/bahmni-concat-config/files/bahmni_concat_config.py
@@ -1,9 +1,10 @@
-import json
 import glob
+import json
 import ntpath
+import os
 
 
-def concat_config(fileName, path):
+def concat_config(fileName, path, offline_path):
     result = {}
     jsonfiles = glob.glob(path + "*.json")
     jsFiles = glob.glob(path + "*.js")
@@ -12,6 +13,10 @@ def concat_config(fileName, path):
             jsonfiles.append(jsfile)
 
     for file in jsonfiles:
+        file_name = ntpath.basename(file)
+        offline_file = glob.glob(offline_path + file_name)
+        if os.path.isfile(offline_path + file_name):
+            file = offline_file[0]
         with open(file, "rb") as infile:
             if ntpath.basename(file).endswith('.json'):
                 file_name = ntpath.basename(file)
@@ -24,11 +29,16 @@ def concat_config(fileName, path):
         json.dump(result, outfile)
 
 
-
-paths = {'home' :'/var/www/bahmni_config/openmrs/apps/home/',
-         'registration': '/var/www/bahmni_config/openmrs/apps/registration/' ,
+paths = {'home': '/var/www/bahmni_config/openmrs/apps/home/',
+         'registration': '/var/www/bahmni_config/openmrs/apps/registration/',
          'clinical': '/var/www/bahmni_config/openmrs/apps/clinical/'
-        }
+         }
+
+offlinePaths = {
+    'home': '/var/www/bahmni_config/offline/openmrs/apps/home/',
+    'registration': '/var/www/bahmni_config/offline/openmrs/apps/registration/',
+    'clinical': '/var/www/bahmni_config/offline/openmrs/apps/clinical/'
+}
 
 for key in paths:
-    concat_config(key, paths[key])
+    concat_config(key, paths[key], offlinePaths[key])
