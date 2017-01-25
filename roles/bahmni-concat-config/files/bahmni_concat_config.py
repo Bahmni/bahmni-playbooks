4e23c9b5998ee4e4ca2c47861a0dc48944141d92
Ujjavala <ujjavala12@gmail.com>
2017-01-25 11:41:12 +0530

    3076 | Ujjavala Singh |db-backup story

--- roles/bahmni-concat-config/files/bahmni_concat_config.py
+++ roles/bahmni-concat-config/files/bahmni_concat_config.py
@@ -8,11 +8,15 @@ def concat_config(fileName, path, offline_path):
     result = {}
     jsonfiles = glob.glob(path + "*.json")
     jsFiles = glob.glob(path + "*.js")
+
     if len(jsFiles) > 0:
         for jsfile in jsFiles:
             jsonfiles.append(jsfile)
+            print "appended json files"
+            
 
     for file in jsonfiles:
+        print file
         file_name = ntpath.basename(file)
         offline_file = glob.glob(offline_path + file_name)
         if os.path.isfile(offline_path + file_name):
@@ -24,19 +28,17 @@ def concat_config(fileName, path, offline_path):
             else:
                 file_name = ntpath.basename(file)
                 result[file_name] = infile.read().replace('\n', '').replace('\t', '')
+                print file_name
 
     with open(path + fileName + ".json", "w+") as outfile:
         json.dump(result, outfile)
 
 
-paths = {'home': '/var/www/bahmni_config/openmrs/apps/home/',
-         'registration': '/var/www/bahmni_config/openmrs/apps/registration/',
+paths = {
          'clinical': '/var/www/bahmni_config/openmrs/apps/clinical/'
          }
 
 offlinePaths = {
-    'home': '/var/www/bahmni_config/offline/openmrs/apps/home/',
-    'registration': '/var/www/bahmni_config/offline/openmrs/apps/registration/',
     'clinical': '/var/www/bahmni_config/offline/openmrs/apps/clinical/'
 }
 
