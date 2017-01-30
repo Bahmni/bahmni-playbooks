43ca7d8ec8104c1dbbb2bcd44605730020f3b5d5
Sourav Agarwal <souravagarwal@Souravs-MacBook-Pro.local>
2017-01-30 16:41:50 +0530

    Sourav | #2972 | Tests to check mysql and psql replication

---
+++ roles/mysql-replication/spec/mysql_replication_spec.rb
@@ -0,0 +1,17 @@
+require "mysql2"
+require 'ansible_spec'
+
+inventory = AnsibleSpec.load_targets(AnsibleSpec.load_ansiblespec[1])
+
+describe "mysql replication" do
+  it "should work" do
+    masterDbHost = inventory["bahmni-emr-db"][0]["uri"]
+    slaveDbHost = inventory["bahmni-emr-db-slave"][0]["uri"]
+    masterDbConnection = Mysql2::Client.new(:host => masterDbHost, :username => "root", :password => "password", :database => "openmrs")
+    slaveDbConnection = Mysql2::Client.new(:host => slaveDbHost, :username => "root", :password => "password", :database => "openmrs")
+    masterDbConnection.query("create table dummy_table (id int)")
+    results = slaveDbConnection.query("select * from information_schema.tables where table_name = 'dummy_table';")
+    expect(results).not_to be_nil
+    masterDbConnection.query("drop table dummy_table")
+  end
+end
---
+++ roles/postgres-replication/spec/postgres_replication_spec.rb
@@ -0,0 +1,36 @@
+require 'pg'
+require 'ansible_spec'
+
+inventory = AnsibleSpec.load_targets(AnsibleSpec.load_ansiblespec[1])
+
+describe "replication" do
+  it "should work in bahmni lab database" do
+    masterDbHost = inventory["bahmni-lab-db"][0]["uri"]
+    slaveDbHost = inventory["bahmni-lab-db-slave"][0]["uri"]
+
+    print masterDbHost
+    print slaveDbHost
+
+    masterDbConnection = PG.connect :host=> masterDbHost, :user => 'postgres', :password=> '', :dbname => 'clinlims'
+    slaveDbConnection = PG.connect :host=> slaveDbHost, :user => 'clinlims', :password=> '', :dbname => 'clinlims'
+    masterDbConnection.query("create table dummy_table (id int)")
+    results = slaveDbConnection.query("select * from information_schema.tables where table_name = 'dummy_table';")
+    expect(results).not_to be_nil
+    masterDbConnection.query("drop table dummy_table")
+    end
+
+  it "should work in openerp database" do
+    masterDbHost = inventory["bahmni-erp-db"][0]["uri"]
+    slaveDbHost = inventory["bahmni-erp-db-slave"][0]["uri"]
+
+    print masterDbHost
+    print slaveDbHost
+
+    masterDbConnection = PG.connect :host=> masterDbHost, :user => 'postgres', :password=> '', :dbname => 'openerp'
+    slaveDbConnection = PG.connect :host=> slaveDbHost, :user => 'openerp', :password=> '', :dbname => 'openerp'
+    masterDbConnection.query("create table dummy_table (id int)")
+    results = slaveDbConnection.query("select * from information_schema.tables where table_name = 'dummy_table';")
+    expect(results).not_to be_nil
+    masterDbConnection.query("drop table dummy_table")
+  end
+end
