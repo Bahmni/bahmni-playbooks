d2442226118a46e4e1302f79ef5f2d656b8af07f
Ujjavala <ujjavala12@gmail.com>
2017-02-14 16:43:48 +0530

    Restore pitr in postgres

--- roles/pgbackrest-restore/files/pitr.sh
+++ roles/pgbackrest-restore/files/pitr.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
 time=$(cat $1/backup_info.txt  |awk -v id="$2" '{if( $3 == id) print $1}')
-backup_time=$(echo $time|awk 'BEGIN { FIELDWIDTHS = "4 2 2 2 2 2" } { printf "%s-%s-%s %s:%s:%s\n", $1, $2, $3, $4, $5, $6 }')
+if [ -z $time ]
+ then
+   echo "Did not find the restore point in backup dir"
+   exit -1
+fi
+backup_time=$(echo $time|awk 'BEGIN { FIELDWIDTHS = "4 2 2 2 2 2" } { printf "%s-%s-%s %s:%s:%s\n", $1, $2, $3, $4, $5+1, $6 }')
 sudo -u postgres pgbackrest --stanza=bahmni-postgres --delta --type=time  "--target=$backup_time"    --set=$2   --log-level-console=detail restore 2>>$3  
