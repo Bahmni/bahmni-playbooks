939e8c66f65ea775a638014a05f6b33a2e40c40f
tbindu <tbindu@intbindu.local>
2017-02-25 23:22:37 +0530

    bindu | Fixed a typo in ansible-tests.yml

--- roles/mysql-replication/spec/mysql_replication_spec.rb
+++ roles/mysql-replication/spec/mysql_replication_spec.rb
@@ -6,19 +6,13 @@ inventory = AnsibleSpec.load_targets(AnsibleSpec.load_ansiblespec[1])
 describe "mysql replication" do
   it "should work" do
     masterDbHost = inventory["bahmni-emr-db"][0]["uri"]
-    begin
       slaveDbHost = inventory["bahmni-emr-db-slave"][0]["uri"]
-    rescue
-      slaveDbHost = 'null'
-    end
 
-    if slaveDbHost != 'null' then
     masterDbConnection = Mysql2::Client.new(:host => masterDbHost, :username => "root", :password => "password", :database => "openmrs")
     slaveDbConnection = Mysql2::Client.new(:host => slaveDbHost, :username => "root", :password => "password", :database => "openmrs")
     masterDbConnection.query("create table dummy_table (id int)")
     results = slaveDbConnection.query("select * from information_schema.tables where table_name = 'dummy_table';")
     expect(results).not_to be_nil
     masterDbConnection.query("drop table dummy_table")
-    end
   end
 end
