c5eb3c25b32cd252778de6f858d6d7f372616063
Asmita Chavan <krishnanspace@gmail.com>
2018-11-06 17:28:01 +0530

    BAH-630: Added changes to support Bahmni Data migration

--- roles/mysql/tasks/mysql_secure_installation.sh
+++ roles/mysql/tasks/mysql_secure_installation.sh
@@ -1,2 +1,5 @@
 password=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $11}')
+echo "innodb_file_format=Antelope" >> /etc/my.cnf
+echo "innodb_default_row_format=COMPACT" >> /etc/my.cnf
+systemctl restart mysqld
 mysql -sfu root -p$password --connect-expired-password < "/opt/bahmni-installer/bahmni-playbooks/roles/mysql/tasks/mysql_secure_installation.sql"
