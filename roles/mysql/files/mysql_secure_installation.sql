09db7d4889b765db45772fd27821ce627bfc0674
krishnanspace <34261449+krishnanspace@users.noreply.github.com>
2019-01-08 17:17:56 +0530

    Password changes for MEDUIM policy (#40)

--- roles/mysql/files/mysql_secure_installation.sql
+++ roles/mysql/files/mysql_secure_installation.sql
@@ -1,7 +1,7 @@
 SET GLOBAL validate_password_number_count=0;
 SET GLOBAL validate_password_length=6;
 SET GLOBAL validate_password_policy=LOW;
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'P@ssw0rd';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
