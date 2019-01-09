0437c7178c7be954e846898c4b43af7a611e4c08
krishnanspace <34261449+krishnanspace@users.noreply.github.com>
2019-01-09 20:21:22 +0530

    BAH-630 | Removed the shell and sql scripts and made changes to the ansible task to perform the same tasks (#41)

--- roles/mysql/files/mysql_secure_installation.sql
+++
@@ -1,9 +0,0 @@
-SET GLOBAL validate_password_number_count=0;
-SET GLOBAL validate_password_length=6;
-SET GLOBAL validate_password_policy=LOW;
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'P@ssw0rd';
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-FLUSH PRIVILEGES;
